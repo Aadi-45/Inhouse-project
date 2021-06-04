d3ad3afdca2aaa3b74981ab0a3808990e1dbdb40
bhavya-gohil <76893298+bhavya-gohil@users.noreply.github.com>
2021-06-04 14:56:49 +0530

    Product medels and view

--- core/ecomm/models.py
+++ core/ecomm/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+SIZE = (
+    ('S', 'S'),
+    ('M', 'M'),
+    ('L', 'L'),
+    ('XL', 'XL'),
+)
+
+CATEGORIES = (
+    ('SH','Shirt'),
+    ('TS','T-Shirt'),
+    ('TR','Trouser'),
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    desc = models.CharField(max_length=500)
+    price = models.FloatField(default=0.00)
+    size = models.CharField(choices=SIZE, max_length=2)
+    category = models.CharField(choices=CATEGORIES, max_length=2)
+    img1 = models.ImageField(upload_to='ecomm/images', default='')
+    img2 = models.ImageField(upload_to='ecomm/images', default='')
+    img3 = models.ImageField(upload_to='ecomm/images', default='')
+
+    def __str__(self):
+        return self.title
--- core/ecomm/views.py
+++ core/ecomm/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, redirect
+from .models import Product
 
 def homeView(request):
     context = {}
     return render(request, 'ecomm/index.html', context)
+
+def product_view(request, id):
+    prod = Product.objects.get(id = id)
+    context = {
+        'prod': prod,
+    }
+    return render(request, 'ecomm/product.html', context)
+
